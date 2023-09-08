4eebdca883a1c2e6aceb0131a9db57c1a684ae18
Glenn Neidermeier <neiderm@github.com>
2023-09-08 18:37:38 -0400

    Clean up example asm code

--- cpu/t80/bldz80.sh
+++ cpu/t80/bldz80.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-TEST_ASM="./test.asm"
+TEST_ASM="./z80.asm"
 echo z80asm $TEST_ASM
 
 z80asm $TEST_ASM
 
-#./t80_comp_vga.srcs/sources_1/new/roms_1.vhd
-od -v  -t x1 a.bin | cut -c 9- | tr '\ ' '\n' > ./t80_comp_vga.srcs/sources_1/new/z80test_hex.dat
+# Old example, dump binary to file, massage it into hex for loading from VHDL code
+# od -v  -t x1 a.bin | cut -c 9- | tr '\ ' '\n' > ./t80_comp_vga.srcs/sources_1/new/z80test_hex.dat
 
-z80dasm  -t a.bin | head -n10
+z80dasm -t a.bin
 
+# array (not synchronized)
+hex2rom -b a.bin prog_rom 6l8a
 
-# hex2rom -b  a.bin  prog_rom 6l8a # array
-# hex2rom -b  a.bin  prog_rom 6l8s # synchronized
+# synchronized
+echo "hex2rom -b a.bin prog_rom 6l8s  | tee ./t80_sim/t80_vga.srcs/sources_1/new/prog_rom.vhd"
 
--- cpu/t80/z80.asm
+++ cpu/t80/z80.asm
@@ -1,19 +1,20 @@
 ; Z80 Test 8/12/2023 RB
 org 0
-  ld hl,ram_start   ;0100   21 00 80
-; xor a             ;       af        a := 0
-  ld  a, 55         ;0103   3e 37     a:=37h     
-loop:
-  ld  (hl),a        ;0105   77
-  inc a             ;0106   3c
-  inc hl            ;0107   23
-  jr  loop          ;0108   18 fb     (jr $-3)
+  ld hl, l_ram_start  ;0100   21 00 80
+; xor a               ;       af        a := 0
+  ld  a, 55           ;0103   3e 37     a:=37h     
+l_loop:
+  ld  (hl), a         ;0105   77
+  inc a               ;0106   3c
+  inc hl              ;0107   23
+  jr  l_loop          ;0108   18 fb     (jr $-3)
 
 org 0x8000
-ram_start:
+l_ram_start:
   db  00
 
 org 0x8400
-top_of_stack:       ; SP is pre-incremented i.e. 87FF is first address push'd
+l_stack_top:          ; SP is pre-incremented i.e. 87FF is first address push'd
 
 gfx_ram:
+
