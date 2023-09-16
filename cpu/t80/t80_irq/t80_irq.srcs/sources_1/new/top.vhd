059994746c6856575569e75bb327f6263da39c75
red-bote <red-bote@red-bote.com>
2023-09-16 06:08:54 -0400

    New VHDL project t80 IRQ

---
+++ cpu/t80/t80_irq/t80_irq.srcs/sources_1/new/top.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/16/2023 05:44:02 AM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           sw : in STD_LOGIC_VECTOR (15 downto 0);
+           led : out STD_LOGIC_VECTOR (15 downto 0));
+end top;
+
+architecture Behavioral of top is
+
+begin
+
+    led <= sw;
+
+end Behavioral;
