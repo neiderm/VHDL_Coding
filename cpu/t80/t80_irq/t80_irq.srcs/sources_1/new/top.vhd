7c3aa66783213bc32e71a0f743c7c1bf4dc00e5e
red-bote <red-bote@red-bote.com>
2023-09-20 08:08:08 -0400

    Chip Select and Data Bus Mux

--- cpu/t80/t80_irq/t80_irq.srcs/sources_1/new/top.vhd
+++ cpu/t80/t80_irq/t80_irq.srcs/sources_1/new/top.vhd
@@ -49,8 +49,19 @@ architecture Behavioral of top is
     signal clk_cpu  : STD_LOGIC;
 
     -- cpu
+    signal cpu_mreq_l     : std_logic;
+    signal cpu_wr_l       : std_logic;
     signal cpu_addr       : std_logic_vector(15 downto 0);
+    signal cpu_data_in    : std_logic_vector(7 downto 0);
+    signal cpu_data_out   : std_logic_vector(7 downto 0);
+
     signal prog_rom_data  : std_logic_vector(7 downto 0);
+    signal ram_data_in    : std_logic_vector(15 downto 0);
+    signal ram_data_out   : std_logic_vector(15 downto 0);
+
+    signal work_rams_we   : std_logic;
+    signal prog_rom_cs_l  : std_logic;
+    signal work_ram_cs_l  : std_logic;
 
     signal video_on : STD_LOGIC;
     signal pixel_x  : INTEGER;
@@ -64,7 +75,7 @@ architecture Behavioral of top is
 
 begin
     --------------------------------------------------
-    -- update IO and other signals
+    -- connect signals
     --------------------------------------------------
     --led <= sw;
     reset_l <= not reset; -- invert active-high reset button
@@ -94,17 +105,47 @@ begin
             NMI_n   => '1', -- cpu_nmi_l,
             BUSRQ_n => '1', -- cpu_busrq_l,
             M1_n    => open, -- cpu_m1_l,
-            MREQ_n  => open, -- cpu_mreq_l,
+            MREQ_n  => cpu_mreq_l,
             IORQ_n  => open, -- cpu_iorq_l,
             RD_n    => open, -- cpu_rd_l,
-            WR_n    => open, --cpu_wr_l,
+            WR_n    => cpu_wr_l,
             -- RFSH_n  => cpu_rfsh_l,
             -- HALT_n  => cpu_halt_l,
             -- BUSAK_n => cpu_busak_l,
             A       => cpu_addr,
-            DI      => prog_rom_data, -- cpu_data_in,
-            DO      => open --cpu_data_out
+            DI      => cpu_data_in,
+            DO      => cpu_data_out
         );
+
+    --------------------------------------------------
+    -- primary addr decode (chip selects)
+    --------------------------------------------------
+    prog_rom_cs_l  <= '0' when cpu_addr(15)           = '0'     else '1'; -- ROM at $0000, RAM at $8000
+    work_ram_cs_l  <= '0' when cpu_addr(15 downto 11) = "10000" else '1'; -- Work RAM at $8000 (1k or 2k)
+    -- cpu data in mux (bus isolation)
+    cpu_data_in  <=
+        prog_rom_data             when prog_rom_cs_l = '0' else
+        ram_data_out(7 downto 0)  when work_ram_cs_l = '0' else
+        x"FF"; -- should never be read by CPU?
+    -- cpu data in mux (bus isolation)
+--    cpu_data_in  <=  prog_rom_data; -- temp
+
+    --------------------------------------------------
+    -- work RAM
+    --------------------------------------------------
+    work_rams_we <= not (cpu_wr_l or cpu_mreq_l); -- WR_n==0 AND MREQ_n==0
+    --work_rams_ce <= not (work_ram_cs_l);
+    ram_data_in(7 downto 0) <= cpu_data_out; -- 16-bit data bus
+
+    u_rams : entity work.rams_08
+      port map (
+        a    => cpu_addr(5 downto 0), -- map RAM to $8000
+        di   => ram_data_in,
+        do   => ram_data_out,
+        we   => work_rams_we, -- write enable, active high
+        en   => '1',   -- chip enable, active high   
+        clk  => clk_cpu
+      );
 
     --------------------------------------------------
     -- internal program ROM
